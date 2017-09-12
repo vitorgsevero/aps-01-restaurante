b51d7e380831741d891c51a63a7cd209c5ee7d9d
Vitor Severo <vitorgsevero@gmail.com>
2017-09-12 20:37:26 -0300

    primeiro commit com a primeira classe
    https://github.com/vitorgsevero/aps-01-restaurante.git

---
+++ src/Pratos.java
@@ -0,0 +1,23 @@
+
+public class Pratos {
+	
+	private String nome;
+	private String descricao;
+	private double preco;
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
+	
+	
+	
+	
+
+}
