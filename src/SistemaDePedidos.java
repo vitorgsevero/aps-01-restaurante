7a86ec4663b7ec1eff8eba96b731c6f92953ed30
Vitor Severo <vitorgsevero@gmail.com>
2017-09-13 00:17:21 -0300

    commit classe principal alterada

--- src/SistemaDePedidos.java
+++ src/SistemaDePedidos.java
@@ -4,7 +4,7 @@ public class SistemaDePedidos {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Restaurante.mostrarCardapio();
+		Restaurante.mostrarCardapio(); //CHAMANDO O MÉTODO PRA VISUALIZAR O CARDÁPIO
 
 	}
 
