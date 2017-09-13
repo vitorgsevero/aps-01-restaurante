6bcf63cbdd3a1d413afca595fc58747172384ca1
Vitor Severo <vitorgsevero@gmail.com>
2017-09-13 15:35:38 -0300

    Mudança na descrição da classe Restaurante

--- src/Ingrediente.java
+++ src/Ingrediente.java
@@ -1,4 +1,8 @@
 
 public class Ingrediente {
 
+	static Ingrediente ingre1 = new Ingrediente ();
+	static Ingrediente ingre2 = new Ingrediente ();
+	static Ingrediente ingre3 = new Ingrediente ();
+
 }
--- src/Restaurante.java
+++ src/Restaurante.java
@@ -8,14 +8,19 @@ public class Restaurante {
 	
 	public static void mostrarCardapio(){
 		
-		System.out.println(prato1.getNome()+": "+ prato1.getPreco());
+		System.out.println("Cardápio: \n");
+		
+		System.out.println(prato1.getNome()+": R$ "+ prato1.getPreco());
 		prato1.setDescricao("Costelinha de porco ao molho barbecue");
+		System.out.println("Costelinha de porco ao molho barbecue\n");
 		
-		System.out.println(prato2.getNome()+": "+prato2.getPreco());
+		System.out.println(prato2.getNome()+": R$ "+prato2.getPreco());
 		prato2.setDescricao("Lasanha de carne com presunto e queijo");
+		System.out.println("Lasanha de carne com presunto e queijo\n");
 		
-		System.out.println(prato3.getNome()+": "+ prato3.getPreco());
+		System.out.println(prato3.getNome()+": R$ "+ prato3.getPreco());
 		prato3.setDescricao("Fondue de carne com molho de queijo e fondue frutas com calda de chocolate");
+		System.out.println("Fondue de carne com molho de queijo e fondue frutas com calda de chocolate\n");
 		
 		
 	}
